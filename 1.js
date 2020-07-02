7bd324f18d5f6ab92919b2a813653a1d6f975ae9
Alfiya Yausheva <al.yausheva@gmail.com>
2020-07-02 22:27:20 +0300

    отменено по умолчанию

--- 1.js
+++ 1.js
@@ -3,6 +3,7 @@ const menu = document.querySelector(".opened__menu");
 const close = document.querySelector(".close-icon");
  
 hamburger.addEventListener("click", function() {
+  e.preventDefault();
   menu.classList.add(".opened__menu--active");
 })
 
