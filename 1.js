43eec054dc2edf3244dfa58f968b3d9844842057
Alfiya Yausheva <al.yausheva@gmail.com>
2020-07-02 21:52:06 +0300

    js

--- 1.js
+++ 1.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".opened__menu");
 const close = document.querySelector(".close-icon");
  
-hamburger.addEventListener("mouseover", function() {
+hamburger.addEventListener("click", function() {
   menu.classList.add(".opened__menu--active");
 })
 
