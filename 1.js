e67c7866f865a3610253f883868cf65ea6e9cecc
Alfiya Yausheva <al.yausheva@gmail.com>
2020-07-04 16:24:30 +0300

    homework

--- 1.js
+++ 1.js
@@ -2,11 +2,11 @@ const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".opened__menu");
 const close = document.querySelector(".close-icon");
  
-hamburger.addEventListener("click", function() {
-  Event.preventDefault();
-  menu.classList.add(".opened__menu--active");
+hamburger.addEventListener("click", function(event) {
+  event.preventDefault();
+  menu.classList.add("opened__menu--active");
 })
 
-close.addEventListener("click", function() {
-  menu.classList.remove(".opened__menu--active");
+close.addEventListener("click", function(event) {
+  menu.classList.remove("opened__menu--active");
 })
