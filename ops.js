a061e82e11ea16a727141e31b33dba03c4fbd568
Alfiya Yausheva <al.yausheva@gmail.com>
2020-07-11 11:15:14 +0300

    18

--- ops.js
+++ ops.js
@@ -52,19 +52,31 @@ $(window).on("wheel", (e) => {
   }
 });
 
-$(window).on("keydown", (e) =>{
+$(window).on("keydown", (e) => {
 
   const tagName = e.target.tagName.toLowerCase();
 
-  if (tagName !== )
-
-  switch (e.keyCode) {
-    case 38:
-      scrollViewport("prev");
-      break;
-  
-      case 40:
-      scrollViewport("next");
-      break;
+  if (tagName !== "input" && tagName !== "textarea") {
+    switch (e.keyCode) {
+      case 38:
+        scrollViewport("prev");
+        break;
+    
+        case 40:
+        scrollViewport("next");
+        break;
+   }
   }
+});
+
+$("[data-scrol-to]").click(e =>{
+  e.preventDefault();
+
+  const $this = $(e.currentTarget);
+  const target = $this.attr("data-scroll-to");
+  const reqSection = $(`[data-section-id=${target}]`);
+
+  perfomTransition(reqSection.index());
+
+
 });
